c1e0a498aeed7e0f00dc7c5887f54adc6faba9c9
Sauljimenez77 <saul111jimenez@gmail.com>
2026-04-05 21:10:00 -0600

    Le agregamos la opcion es de suma, resta, multiplicacion, division.

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tempVal;
     Button btn;
-    Spinner spn;m
+    Spinner spn;l
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
