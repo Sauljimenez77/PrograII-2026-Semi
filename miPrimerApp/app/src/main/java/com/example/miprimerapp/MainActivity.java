379b33aafbdc44964d92867c7eddad6a1559b8d9
Sauljimenez77 <saul111jimenez@gmail.com>
2026-04-05 21:11:30 -0600

    Usando el RadioGroup y el Id para saber cual operacion es la que se va a realizar

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tempVal;
     Button btn;
-    Spinner spn;l
+    Spinner spn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
